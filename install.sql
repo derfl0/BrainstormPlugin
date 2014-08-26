5e162c3bd65c2c303ac8f56245a884c4c570b1ef
Rasmus Fuhse <fuhse@data-quest.de>
2014-08-26 13:22:03 +0200

    display icon in course overview

--- install.sql
+++ install.sql
@@ -3,6 +3,8 @@ CREATE TABLE `brainstorm_votes` (
   `brainstorm_id` varchar(32) NOT NULL DEFAULT '',
   `user_id` varchar(32) NOT NULL DEFAULT '',
   `vote` tinyint(4) DEFAULT NULL,
+  `chdate` BIGINT NOT NULL,
+  `mkdate` BIGINT NOT NULL,
   PRIMARY KEY (`brainstorm_id`,`user_id`)
 );
 
@@ -13,6 +15,8 @@ CREATE TABLE `brainstorms` (
   `range_id` varchar(32) NOT NULL DEFAULT '',
   `title` varchar(255) DEFAULT NULL,
   `text` text NOT NULL,
+  `chdate` BIGINT NOT NULL,
+  `mkdate` BIGINT NOT NULL,
   PRIMARY KEY (`brainstorm_id`),
   KEY `range_id` (`range_id`)
 );
